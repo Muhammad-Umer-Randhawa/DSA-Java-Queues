a1ba0c55aacd505f5f5585529624b3a35320d1c4
Muhammad Umer <muhammadumer65659@gmail.com>
2026-09-03 08:44:08 -0700

    Made the remove from index method in queue

--- Remove.java
+++ Remove.java
@@ -1,20 +1,20 @@
 import java.util.Queue;
 import java.util.LinkedList;
 
-public class AddInAQueue {
+public class Remove {
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
         q.add(10); q.add(20); q.add(30); q.add(40);
-        addInQueue(q, 25, 2);
+        removeFromQueue(q, 2);
         printQueue(q);
     }
-    // adds a value at an index in a queue, index starts from 0
-    private static void addInQueue(Queue<Integer> q, int val, int idx){
-        int n = q.size();
+    // removes a value at an index in a queue, index starts from 0
+    private static void removeFromQueue(Queue<Integer> q, int idx){
         for(int i=1; i<=idx; i++){
             q.add(q.remove());
         }
-        q.add(val);
+        q.remove();
+        int n = q.size();
         for(int i=1; i<=n-idx; i++){
             q.add(q.remove());
         }
